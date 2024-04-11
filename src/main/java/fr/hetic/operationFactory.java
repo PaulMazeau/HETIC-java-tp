a5f073038e91703560cafbf5fb72a049f6e0da37
Paul MAZEAU <mazeau.paul@gmail.com>
2024-04-11 10:30:57 +0200

    Exercice 5 qui compile bien

--- src/main/java/fr/hetic/operationFactory.java
+++ src/main/java/fr/hetic/operationFactory.java
@@ -2,15 +2,17 @@ import java.util.Map;
 import java.util.function.BinaryOperator;
 
 public class OperationFactory {
-    private static final Map<String, BinaryOperator<Double>> OPERATIONS = Map.of(
-        "+", (a, b) -> a + b,
-        "-", (a, b) -> a - b,
-        "*", (a, b) -> a * b
-    );
-
-    public static BinaryOperator<Double> getOperation(String operator) {
-        return OPERATIONS.getOrDefault(operator, (a, b) -> {
-            throw new IllegalArgumentException("Opérateur non supporté: " + operator);
-        });
+    public static OperationStrategy getOperationStrategy(String operator) {
+        switch (operator) {
+            case "+":
+                return new AdditionStrategy();
+            case "-":
+                return new SubtractionStrategy();
+            case "*":
+                return new MultiplicationStrategy();
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
     }
 }
+
